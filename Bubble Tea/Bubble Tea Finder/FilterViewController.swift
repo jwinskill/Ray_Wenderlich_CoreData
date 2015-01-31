391b4cca6a3968395882adefc29a9fc10e5357c0
Josh Winskill <jwinskill@gmail.com>
2015-01-30 16:41:24 -0800

    Completed CoreData Chapter 4, completed Bubble Tea app with batch requests and asynchronous fetch requests

--- Bubble Tea/Bubble Tea Finder/FilterViewController.swift
+++ Bubble Tea/Bubble Tea Finder/FilterViewController.swift
@@ -9,8 +9,16 @@
 import UIKit
 import CoreData
 
+protocol FilterViewControllerDelegate: class {
+  func filterViewController(filter: FilterViewController, didSelectPredicate predicate: NSPredicate?, sortDescriptor: NSSortDescriptor?)
+}
+
 class FilterViewController: UITableViewController {
   
+  //FilterViewController Delegate section
+  weak var delegate: FilterViewControllerDelegate?
+  var selectedSortDescriptor: NSSortDescriptor?
+  var selectedPredicate: NSPredicate?
   
   //Outlets section
   @IBOutlet weak var firstPriceCategoryLabel: UILabel!
@@ -48,6 +56,32 @@ class FilterViewController: UITableViewController {
     var predicate = NSPredicate(format: "priceInfo.priceCategory == %@", "$$$")
     return predicate!
   }()
+  lazy var offeringDealPredicate: NSPredicate = {
+    var predicate = NSPredicate(format: "specialCount > 0")
+    return predicate!
+  }()
+  lazy var walkingDistancePredicate: NSPredicate = {
+    var predicate = NSPredicate(format: "location.distance < 500")
+    return predicate!
+  }()
+  lazy var hasUserTipsPredicate: NSPredicate = {
+    var predicate = NSPredicate(format: "stats.tipCount > 0")
+    return predicate!
+  }()
+  
+  //Sort Descriptor section
+  lazy var nameSortDescriptor: NSSortDescriptor = {
+    var sortDescriptor = NSSortDescriptor(key: "name", ascending: true, selector: "localizedStandardCompare:")
+    return sortDescriptor
+  }()
+  lazy var distanceSortDescriptor: NSSortDescriptor = {
+    var sortDescriptor = NSSortDescriptor(key: "location.distance", ascending: true)
+    return sortDescriptor
+  }()
+  lazy var priceSortDescriptor: NSSortDescriptor = {
+    var sortDescriptor = NSSortDescriptor(key: "priceInfo.priceCategroy", ascending: true)
+    return sortDescriptor
+  }()
   
   //MARK - overridden view controller methods
   
@@ -56,17 +90,49 @@ class FilterViewController: UITableViewController {
     populateCheapMenuCountLabel()
     populateModerateMenuCountLabel()
     populateExpensiveMenuCountLabel()
+    populateDealsCountLabel()
   }
 
   //MARK - UITableViewDelegate methods
   
   override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
     
+    let cell = self.tableView.cellForRowAtIndexPath(indexPath)!
+    
+    switch cell {
+      // price section
+    case cheapVenueCell:
+      selectedPredicate = self.cheapVenuePredicate
+    case moderateVenueCell:
+      selectedPredicate = self.moderateVenuePredicate
+    case expensiveVenueCell:
+      selectedPredicate = self.expensiveVenuePredicate
+    case offeringDealCell:
+      selectedPredicate = self.offeringDealPredicate
+    case walkingDistanceCell:
+      selectedPredicate = self.walkingDistancePredicate
+    case userTipsCell:
+      selectedPredicate = self.hasUserTipsPredicate
+    case nameAZSortCell:
+        selectedSortDescriptor = self.nameSortDescriptor
+    case nameZASortCell:
+        selectedSortDescriptor = self.nameSortDescriptor.reversedSortDescriptor as? NSSortDescriptor
+    case distanceSortCell:
+        selectedSortDescriptor = self.distanceSortDescriptor
+    case priceSortCell:
+        selectedSortDescriptor = self.priceSortDescriptor
+    default:
+      println("default case")
+    }
+    
+    cell.accessoryType = .Checkmark
   }
   
   // MARK - UIButton target action
   
   @IBAction func saveButtonTapped(sender: UIBarButtonItem) {
+    
+    self.delegate!.filterViewController(self, didSelectPredicate: self.selectedPredicate, sortDescriptor: self.selectedSortDescriptor)
     
     dismissViewControllerAnimated(true, completion:nil)
   }
@@ -119,6 +185,35 @@ class FilterViewController: UITableViewController {
     }
     
     self.thirdPriceCategoryLabel.text = "\(count) bubble tea places"
+  }
+  
+  func populateDealsCountLabel() {
+    // 1. Create a fetch request, and set its result type to DictionaryResultType
+    var fetchRequest = NSFetchRequest(entityName: "Venue")
+    fetchRequest.resultType = .DictionaryResultType
+    
+    // 2. create a NSExpressionDescription to request the sum, give it a name so its result can be read
+    let sumExpressionDesc = NSExpressionDescription()
+    sumExpressionDesc.name = "sumDeals"
+    
+    // 3. Specify the function used (sum) and proptery to sum (specialCount), and set return type
+    sumExpressionDesc.expression = NSExpression(forFunction: "sum:", arguments: [NSExpression(forKeyPath: "specialCount")])
+    sumExpressionDesc.expressionResultType = .Integer32AttributeType
+    
+    // 4. Tell the fetch request to fetch the expression we just created
+    fetchRequest.propertiesToFetch = [sumExpressionDesc]
+    
+    // 5. Execute the fetch request
+    var error: NSError?
+    let result = self.coreDataStack.context.executeFetchRequest(fetchRequest, error: &error) as? [NSDictionary]
+    
+    if let resultArray = result {
+      let resultDict = resultArray[0]
+      let numDeals: AnyObject? = resultDict["sumDeals"]
+      self.numDealsLabel.text = "\(numDeals!) total deals"
+    } else {
+      println("Error: \(error?.localizedDescription)")
+    }
   }
   
 }
--- Bubble Tea/Bubble Tea Finder/ViewController.swift
+++ Bubble Tea/Bubble Tea Finder/ViewController.swift
@@ -9,20 +9,72 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FilterViewControllerDelegate {
   
   @IBOutlet weak var tableView: UITableView!
   var coreDataStack: CoreDataStack!
+  var asyncFetchRequest: NSAsynchronousFetchRequest!
   var fetchRequest: NSFetchRequest!
-  var venues: [Venue]!
+  var venues: [Venue] = []
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.fetchRequest = self.coreDataStack.model.fetchRequestTemplateForName("FetchRequest")
     
-    self.fetchAndReload()
-
+    let batchUpdate = NSBatchUpdateRequest(entityName: "Venue")
+    batchUpdate.propertiesToUpdate = ["favorite": NSNumber(bool: true)]
+    batchUpdate.affectedStores = self.coreDataStack.psc.persistentStores
+    batchUpdate.resultType = NSBatchUpdateRequestResultType.UpdatedObjectsCountResultType
+    
+    var batchError: NSError?
+    let batchResult = self.coreDataStack.context.executeRequest(batchUpdate, error: &batchError) as NSBatchUpdateResult?
+    
+    if let result = batchResult {
+      println("Records updated \(result.result!)")
+    } else {
+      println("Could not update \(batchError), \(batchError!.userInfo)")
+    }
+    
+    // 1. Create a fetch request to be wrapped by an asynchronous fetch request
+    var fetchRequest = NSFetchRequest(entityName: "Venue")
+    
+    // 2. Create an asyncfetchrequest with the previous fetch request. Set results and reload table view in completion block
+    self.asyncFetchRequest = NSAsynchronousFetchRequest(fetchRequest: fetchRequest)
+      { [unowned self] (result: NSAsynchronousFetchResult!) -> Void in
+      
+      self.venues = result.finalResult as [Venue]
+      self.tableView.reloadData()
+    }
+    
+    // 3. use the coreData context to call executeRequest (instead of executeFetchRequest) to fetch data
+    var error: NSError?
+    let results = self.coreDataStack.context.executeRequest(self.asyncFetchRequest, error: &error)
+    
+    if let persistentStoreResults = results {
+      
+    } else {
+      println("Could not fetch \(error), \(error!.localizedDescription)")
+    }
   }
+  
+  // MARK: - FilterViewController Methods
+  
+  func filterViewController(filter: FilterViewController, didSelectPredicate predicate: NSPredicate?, sortDescriptor: NSSortDescriptor?) {
+    
+    fetchRequest.predicate = nil
+    fetchRequest.sortDescriptors = nil
+    
+    if let fetchPredicate = predicate {
+      fetchRequest.predicate = fetchPredicate
+    }
+    
+    if let fetchSortDescriptor = sortDescriptor {
+      fetchRequest.sortDescriptors = [fetchSortDescriptor]
+    }
+    
+    fetchAndReload()
+  }
+  
+  // MARK: - TableView Methods
   
   func tableView(tableView: UITableView?,
     numberOfRowsInSection section: Int) -> Int {
@@ -50,6 +102,7 @@ class ViewController: UIViewController {
       let navController = segue.destinationViewController as UINavigationController
       let filterVC = navController.topViewController as FilterViewController
       filterVC.coreDataStack = self.coreDataStack
+      filterVC.delegate = self
     }
   }
     
